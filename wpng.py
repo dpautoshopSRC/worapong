3754dc0d0519cdd0b5aaf7f32046026b2bb18f2c
dpautoshopSRC <108377941+dpautoshopSRC@users.noreply.github.com>
2024-08-21 16:01:46 +0700

    wpng.py

--- wpng.py
+++ wpng.py
@@ -1,7 +1,4 @@
-import streamlit as st
-import seaborn as sns
-from sklearn.model_selection import train_test_split
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import accuracy_score
-from sklearn.svm import SVC
-iris = sns.load_dataset('iris')
+x = 4*np.random.rand(100)
+y = np.sin(2*x + 1) + 0.1* np.random.randn(100)
+
+plt.scatter(x, y)
