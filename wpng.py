ed75d37cdfbb3be88c410fce83c094058107f195
dpautoshopSRC <108377941+dpautoshopSRC@users.noreply.github.com>
2024-08-21 14:56:19 +0700

    wpng.py

--- wpng.py
+++ wpng.py
@@ -1,3 +1,7 @@
-# load iris dataset from seaborn
+import streamlit as st
 import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.svm import SVC
 iris = sns.load_dataset('iris')
